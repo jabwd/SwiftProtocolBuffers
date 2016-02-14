830cff88f0fbd3e9cca0854d83f9e92da9592f49
Antwan van Houdt <jabwd@exurion.com>
2016-02-14 16:49:42 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "ProtocolBuffers",
+    targets: [],
+    dependencies: [
+		//.Package(url: "../SwiftFCGI", versions: Version(0,0,3)..<Version(1,0,0))
+	]
+)
